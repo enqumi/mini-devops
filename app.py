ed5714b8d36f22fd39eaa0abc10aacb2c6d2da0e
xerodmn <beksultanasan21@gmail.com>
2025-12-29 16:45:26 +0500

    design

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, DevOps World!"
-    
+    return render_template('index.html')
 
 @app.route('/health')
 def health():
